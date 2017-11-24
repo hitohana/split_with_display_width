00e9aa71274e2de107f9a45da922d9f61a6b943a
nyaahara <d.niihara@gmail.com>
2017-11-25 00:20:59 +0800

    initial commit

---
+++ lib/split_with_display_length.rb
@@ -0,0 +1,5 @@
+require "split_with_display_length/version"
+
+module SplitWithDisplayLength
+  # Your code goes here...
+end
---
+++ lib/split_with_display_length/version.rb
@@ -0,0 +1,3 @@
+module SplitWithDisplayLength
+  VERSION = "0.1.0"
+end
