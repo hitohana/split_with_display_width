330fb9fe1661df02f50ae49248638639d1dc977b
nyaahara <d.niihara@gmail.com>
2017-11-25 00:37:45 +0800

    initial product code

--- lib/split_with_display_length.rb
+++ lib/split_with_display_length.rb
@@ -1,5 +1,36 @@
-require "split_with_display_length/version"
+class String
+  def split_with_display_length_from_start(num)
+    return [self, ''] if num >= self.display_length
 
-module SplitWithDisplayLength
-  # Your code goes here...
+    first = []
+    second = []
+    self.each_char.each do |char|
+      tmp = first + [char]
+      if tmp.join.display_length <= num
+        first << char
+      else
+        second << char
+      end
+    end
+
+    [first.join, second.join]
+  end
+
+  def split_with_display_length_from_end(num)
+    return ['', self] if num >= self.display_length
+
+    first = []
+    second = []
+    self.each_char.to_a.reverse.each do |char|
+      tmp = second + [char]
+      if tmp.join.display_length <= num
+        second << char
+      else
+        first << char
+      end
+    end
+
+    [first.reverse.join, second.reverse.join]
+  end
 end
+
